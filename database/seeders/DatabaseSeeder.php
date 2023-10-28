9d134704a392af14bd0232e80dbfc15e9e596ead
Arthur Estevão <arthurestevao631@gmail.com>
2023-10-28 10:46:42 -0300

    Added systems schemas to db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             EventSeeder::class,
+            SystemSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/SystemSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Database\Factories\SystemFactory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SystemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        $data = [
+            ['name' => 'Dungeons and Dragons', 'genre' => 'Medieval Fantasy', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Pathfinder', 'genre' => 'Medieval Fantasy', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Tormenta 20', 'genre' => 'Medieval Fantasy', 'created_at' => now(), 'updated_at' => now()],
+            ['name'=> 'Starfinder', 'genre'=> 'Sci-fi', 'created_at' => now(), 'updated_at' => now()],
+            ['name'=> 'Cyberpunk 2077', 'genre'=> 'Sci-fi', 'created_at' => now(), 'updated_at' => now()],
+            ['name'=> 'Tephra', 'genre'=> 'Steampunk', 'created_at' => now(), 'updated_at' => now()],
+            ['name'=> 'Vampire Msasquerade', 'genre'=> 'Dark Fantasy', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Call of Cthulhu', 'genre' => 'Dark Fantasy', 'created_at' => now(), 'updated_at' => now()]
+        ];
+        
+        DB::table('systems')->insert($data);
+    }
+}
