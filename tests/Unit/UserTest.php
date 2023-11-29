ecf62176b80f91843b2bd7c6279e61d5edd7b225
ArthurEstevaum <arthurestevao631@gmail.com>
2023-11-29 17:04:00 -0300

    Update UserTest.php
    Updated UserTest to check if the relation between the user which owns the tabletop and the tabletop itself is correct.

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,8 +2,13 @@
 
 namespace Tests\Unit;
 
+use App\Models\Tabletop;
+use App\Models\User;
+use Database\Seeders\ScenarioSeeder;
+use Database\Seeders\SystemSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
 
@@ -19,7 +24,26 @@ class UserTest extends TestCase
         $this->assertTrue(
             Schema::hasColumns('users', ['id', 'name', 'email', 'admin', 'email_verified_at', 'password',
             'provider_id', 'provider_avatar', 'provider_name', 'remember_token', 'created_at',
-            'updated_at']), 1
+            'updated_at'])
         );
+    }
+
+    /**
+     * Check if the relation between the user which owns
+     * the tabletop and the tabletop itself is correct.
+     */
+    public function test_user_owns_a_tabletop() : void
+    {
+        $user = User::factory()->create();
+
+        $systemSeeder = new SystemSeeder();
+        $systemSeeder->run();
+
+        $scenarioSeeder = new ScenarioSeeder();
+        $scenarioSeeder->run();
+        
+        $tabletop = Tabletop::factory()->create(['owner_user_id' => $user->id]);
+
+        $this->assertInstanceOf(Tabletop::class, $user->owns_tabletops()->find(1));
     }
 }
