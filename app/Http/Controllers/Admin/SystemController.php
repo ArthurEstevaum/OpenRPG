5aab2f0ae39bbae4e396662561033e46a5c138cc
Arthur Estevão de Lima <91440878+ArthurEstevaum@users.noreply.github.com>
2023-12-28 16:53:54 -0300

    Update SystemController.php
    Removed unused facade import.

--- app/Http/Controllers/Admin/SystemController.php
+++ app/Http/Controllers/Admin/SystemController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\SystemResource;
 use App\Models\System;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
