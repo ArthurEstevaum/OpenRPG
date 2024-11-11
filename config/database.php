07c8f57856e4366de59b0ce1d9bef8a34af2a8b6
Arthur Estevão <arthurestevao631@gmail.com>
2024-11-11 13:16:52 -0300

    changed session driver to redis

--- config/database.php
+++ config/database.php
@@ -137,6 +137,14 @@ return [
             'database' => env('REDIS_DB', '0'),
         ],
 
+        'session' => [
+                    'url' => env('REDIS_URL'),
+                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'password' => env('REDIS_PASSWORD', null),
+                    'port' => env('REDIS_PORT', '6379'),
+                    'database' => '0',
+        ],
+
         'cache' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
